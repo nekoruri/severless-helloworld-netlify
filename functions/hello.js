2b31f72277f9aef31722dbcc57a51eb00102d5be
NAKAYAMA Masahiro <aki@nekoruri.jp>
2020-12-16 05:00:42 +0900

    add files

---
+++ functions/hello.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message: `Hello ${req.query.name || req.body.name || ''}!`}),
+  };
+};
